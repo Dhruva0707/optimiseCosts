50291b1a42686759cd3dd5533ca3c94d8cb61eb4
Dhruva0707 <dhruvab5@gmail.com>
2019-05-01 07:25:17 +0530

    TITLE: INITIALISE PACKAGE
    DESCRIPTION:
    1. Ros package optimise was created
    2. catkin-make has been used to initialise the package

---
+++ build/optimise/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dhruva/optimiseCosts/src/optimise
+# Build directory: /home/dhruva/optimiseCosts/build/optimise
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
